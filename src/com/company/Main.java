877393108ca34bcb22f9e384f3235ab22aa35e95
Sergey <serg.karpik@gmail.com>
2022-09-26 21:50:53 +0300

    finished Vasya's task!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,39 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
 
-
-    public static int[] bubbleSort(int[] array)
-    {
+    public static int[] bubbleSort(int[] array) {
         boolean sorted = false;
         int temp;
-        while (!sorted)
-        {
+        while (!sorted) {
             sorted = true;
-            for(int i = 0; i < array.length - 1; i++)
-            {
-                if(array[i] > array[i+1])
-                {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
                     temp = array[i];
-                    array[i] = array[i+1];
-                    array[i+1] = temp;
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
                     sorted = false;
                 }
             }
         }
         return array;
     }
+
     //                  0  1  2   3   4  5   6   7   8    9
     static int[] arr = {6, 2, 15, 5, 15, 81, 4, 100, 245, 253};
 
 
-    public static int[] insertionSort(int[] array){
-        for(int i = 1; i < array.length; i++){
+    public static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
             int current = array[i]; // 4
             int j = i - 1; // 1
-            while (j >= 0 && current < array[j]){
-                array[j+1] = array[j];
+            while (j >= 0 && current < array[j]) {
+                array[j + 1] = array[j];
                 j--;
+
             }
             array[j + 1] = current;
         }
@@ -44,90 +41,121 @@ public class Main {
         return array;
     }
 
-    public static int binarySearch(int[] array, int elementToSearch)
-    {
+    // {0,1,12312,0}
+
+    public static int binarySearch(int[] array, int elementToSearch) {
         int firstElem = 0;
         int lastElem = array.length - 1;
-        while (firstElem <= lastElem)
-        {
+        while (firstElem <= lastElem) {
             int middle = (firstElem + lastElem) / 2;
-            if(array[middle] == elementToSearch)
-            {
+            if (array[middle] == elementToSearch) {
                 return middle;
-            }
-            else if (array[middle] < elementToSearch)
-            {
+            } else if (array[middle] < elementToSearch) {
                 firstElem = middle + 1;
-            }
-            else if (array[middle] > elementToSearch)
-            {
+            } else if (array[middle] > elementToSearch) {
                 lastElem = middle - 1;
             }
         }
         return -1;
     }
 
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            // замена
+            int temp = array[i];
+            array[i] = min;
+            array[minId] = temp;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 
     //5 * 4 * 3 * 2 * 1
-    public static int getFact(int toCalc)
-    {
-        if(toCalc == 1)
-        {
+    public static int getFact(int toCalc) {
+        if (toCalc == 1) {
             return 1;
         }
-        return toCalc * getFact(toCalc-1);
+        return toCalc * getFact(toCalc - 1);
     }
 
     public static void main(String[] args) {
 
+        int salaries[] = new int[4];
 
-        // создание, инициализация, вывод, поиск
-        int a [] = new int[14];
+        Random random = new Random();
+        int min = 15;
+        int max = 1000;
+        int createSalary = 0;
 
-        for(int i = 0; i < a.length; i++)
-        {
-            a[i] = i;
+        for (int i = 0; i < 4; i++) {
+            createSalary = min + random.nextInt(max - min);
+            System.out.println(createSalary);
+            salaries[i] = createSalary;
+        }
+        System.out.println("Before: ");
+        for (int a = 0; a < salaries.length; a++) {
+            System.out.println(salaries[a]);
         }
 
-        for (int i = 0; i < a.length; i++)
-        {
-            System.out.println(a[i]);
+        int minIndx = getElemIndx(salaries, SORT_ORDER.GET_MIN);
+        int maxIndx = getElemIndx(salaries, SORT_ORDER.GET_MAX);
+
+
+        int temp = salaries[minIndx];
+        salaries[minIndx] = salaries[maxIndx];
+        salaries[maxIndx] = temp;
+
+        System.out.println("Now: ");
+        for (int a = 0; a < salaries.length; a++) {
+            System.out.println(salaries[a]);
+        }
+    }
+
+    enum SORT_ORDER {
+        GET_MIN,
+        GET_MAX
+    }
+
+
+    /**
+     * This function returns min/max array's element id
+     *
+     * @param array
+     * @param sortOder (1 - get min array's, other values - max)
+     * @return Array's index depending on input sortOrder
+     */
+    public static int getElemIndx(int array[], SORT_ORDER sortOder) {
+        int requiredIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int elementToSearch = array[0];
+
+            if (sortOder == SORT_ORDER.GET_MIN) {
+                if (array[i] < elementToSearch) {
+                    elementToSearch = array[i];
+                    requiredIndex = i;
+                }
+            } else if (sortOder == SORT_ORDER.GET_MAX){
+                if (array[i] > elementToSearch) {
+                    elementToSearch = array[i];
+                    requiredIndex = i;
+                }
+            }
+            else {
+                System.out.println("Wrong sort order!");
+                return -1;
+            }
         }
 
-        for (int i = 0; i < a.length; i++)
-        {
-           if(a[i] == 4)
-           {
-               System.out.println("a[" + i + "] = " + a[i] );
-           }
-        }
-
-
-
-
-
-
-        //Поиск элемента в отсортированном массиве через алгоритм бинарного поиска
-
-        //System.out.println(getFact(6));
-        /*
-        arr = insertionSort(arr);
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("arr[" + i + "] = " + arr[i]);
-        }
-
-        int result = binarySearch(arr, 2);
-
-        if(result != -1)
-        {
-            System.out.println("Elem was found! Indx " + result);
-        }
-        else
-        {
-            System.out.println("Elem was not found!");
-        }
-
-         */
+        return requiredIndex;
     }
 }
